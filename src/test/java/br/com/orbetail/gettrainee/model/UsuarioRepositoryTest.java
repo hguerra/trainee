b859276ce3892dabe97a135dbc88fe6bbb4a225c
hguerra <heitorguerrac@gmail.com>
2016-05-05 11:29:08 -0300

    initialize project

---
+++ src/test/java/br/com/orbetail/gettrainee/model/UsuarioRepositoryTest.java
@@ -0,0 +1,17 @@
+package br.com.orbetail.gettrainee.model;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author heitor
+ * @since 05/05/16.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/applicationContext.xml"})
+@Transactional
+public class UsuarioRepositoryTest {
+
+}
